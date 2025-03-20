85afe410dbbd3e15fa8e8260e5c8e6f61b080f5d
Jonathan <jonigl@gmail.com>
2025-03-20 01:51:39 +0100

    [main] updating vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/ai-home-redesign/',
-});
+  base: mode === 'production' ? '/ai-home-redesign/' : '/',
+}));
